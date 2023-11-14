79829b1c66e2ab02f71bb418b714eac8adc76104
jeongho <qkrjh0904@gmail.com>
2023-11-15 00:37:15 +0900

    git init

---
+++ src/main/java/com/complete/rt/RtBeApplication.java
@@ -0,0 +1,13 @@
+package com.complete.rt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/complete/rt/RtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complete.rt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
